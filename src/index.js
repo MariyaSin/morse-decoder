28d83dc8016e3c1ec0f23931850747d97ce2e9f2
MariyaSin <maria1991@list.ru>
2020-10-10 00:35:20 +0300

    feat: fixed with decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    let symb = '';
+    let string = '';
+
+    for (let i = 0; i < expr.length - 1; i = i + 2){
+        if (expr[i] == '*'){
+            string += ' ';
+            i += 10;
+        }
+        if (expr[i] === '1'){
+            expr[i+1] === '0' ? (symb += '.'): (symb += '-');
+        }
+        if ((i+2) % 10 === 0){
+            string += MORSE_TABLE[symb];
+            symb = '';
+        }
+    }
+    return string;
 }
 
 module.exports = {
